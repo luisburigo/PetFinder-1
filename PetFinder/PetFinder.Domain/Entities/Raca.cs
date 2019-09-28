7fb1e635264edc9b03e948627573609d02ac93a1
Vitor Pereira Scheffer <vitorps@windowslive.com>
2019-09-28 10:02:11 -0300

    Comit inicial

---
+++ PetFinder/PetFinder.Domain/Entities/Raca.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetFinder.Domain.Entities
+{
+    public class Raca
+    {
+        public int RacaId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+    }
+}
